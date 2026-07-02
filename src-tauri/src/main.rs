c3c7bc78439f3362bafb73d6d22f5af2730369f0
tiendung4411 <tiendung4411@gmail.com>
2026-07-02 13:27:39 +0700

    fix terminal

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     if bilibili_downloader_lib::should_run_native_messaging() {
         if let Err(error) = bilibili_downloader_lib::run_native_messaging() {
